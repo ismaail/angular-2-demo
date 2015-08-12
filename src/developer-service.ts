265402693973967c4ee98d2ed4ffd7d4c5119e05
E.G. ismaail <contact@ismaail.com>
2015-08-12 17:41:49 +0100

    init commit

---
+++ src/developer-service.ts
@@ -0,0 +1,14 @@
+/**
+ * Developer Service Class
+ */
+class DeveloperService {
+    name: string;
+    skills: Array<string>
+
+    constructor() {
+        this.name = "ismaail";
+        this.skills = ["PHP", "NodeJS", "GIT", "Linux"];
+    }
+}
+
+export { DeveloperService };
---
+++ src/main.ts
@@ -0,0 +1,65 @@
+import {
+    Component,
+    View,
+    bootstrap,
+    NgFor
+} from "angular2/angular2";
+
+import { DeveloperService } from "./developer-service";
+
+@Component({
+    selector: "my-app",
+    appInjector: [DeveloperService]
+})
+@View({
+    templateUrl: "templates/app.html",
+    directives: [NgFor]
+})
+
+/**
+ * AppComponenet class
+ */
+class AppComponent {
+    /**
+     * Initialse AppComponent Class
+     *
+     * @param {DeveloperService} developerService
+     */
+    constructor(developerService: DeveloperService) {
+        this.name = developerService.name;
+        this.skills = developerService.skills;
+    }
+
+    /**
+     * Add new skill to the skills list
+     *
+     * @param {object} skill
+     */
+    addToSkills(skill: object) {
+        /**
+         * @type {string}
+         */
+        let newSkill = skill.value.trim();
+
+        if ("" === newSkill) {
+            return;
+        }
+
+        this.skills.push(newSkill);
+        skill.value = null;
+    }
+
+    /**
+     * Listen to new skill input text keyup event
+     *
+     * @param {object} event
+     */
+    doneSkillTyping(event: object) {
+        // Watch for "Enter" key 13
+        if (13 === event.which) {
+            this.addToSkills(event.target);
+        }
+    }
+}
+
+bootstrap(AppComponent);
