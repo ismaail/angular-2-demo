e2a2d3215ba0dc3cd5f228800715d718a3e60491
E.G. ismaail <contact@ismaail.com>
2016-09-07 00:20:49 +0100

    update main app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
     selector: '#my-app',
     template: `
         <h1>Angular 2 Beta - Demo App</h1>
         <router-outlet></router-outlet>
-    `,
-    directives: [ROUTER_DIRECTIVES]
+    `
 })
 
 /**
