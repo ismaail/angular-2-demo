2bfcabca1af2e2cd29ff9ac0bb753b45b6ea92ba
E.G. ismaail <contact@ismaail.com>
2016-09-07 00:19:58 +0100

    update DeveloperService to return Promise in getSkills method

--- app/services/developer.ts
+++ app/services/developer.ts
@@ -1,20 +1,41 @@
-import { Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+import { Skill } from '../models/skill';
 
 /**
  * Developer Service Class
  */
+@Injectable()
 export class DeveloperService {
-    http: Http;
-
     /**
      * Initialse DeveloperService Class
      */
-    constructor(@Inject(Http) http: Http) {
+    constructor(private http: Http) {
         this.http = http;
     }
 
-    load() {
-        return this.http.get('http://localhost:3000/data/skills.json');
+    /**
+     * Get Developer Skills List.
+     * @returns {Promise<Skill[]>}
+     */
+    getSkills(): Promise<Skill[]> {
+        const skillsUrl = 'http://localhost:3000/data/skills.json';
+
+        return this.http.get(skillsUrl)
+            .toPromise()
+            .then(response => response.json().data.skills as Skill[])
+            .catch(this.handleError);
+    }
+
+    /**
+     * Handle Http Get Error
+     * @param error
+     * @returns {Promise<void>|Promise<T>}
+     */
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
