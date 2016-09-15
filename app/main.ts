2cf9d032994751e15ce4739934289905185d559a
E.G. ismaail <contact@ismaail.com>
2016-09-15 21:47:18 +0100

    Remove commented import statements

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { HTTP_PROVIDERS } from '@angular/http';
-// import { appRouterProviders } from './app.routes';
 import { AppModule } from './app.module';
 
 const platform = platformBrowserDynamic();
