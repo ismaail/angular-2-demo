6145c10a9ca4e7a2ec5101a44ea182cce9c957a1
E.G. ismaail <contact@ismaail.com>
2016-12-12 00:36:27 +0000

    Update DeveloperService to return Observable

--- app/services/developer.ts
+++ app/services/developer.ts
@@ -1,8 +1,9 @@
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
 
-import { Skill } from '../models/skill';
+import { Developer } from '../models/developer';
 
 /**
  * Developer Service Class
@@ -18,24 +19,12 @@ export class DeveloperService {
 
     /**
      * Get Developer Skills List.
-     * @returns {Promise<Skill[]>}
+     * @returns {Observable<Developer>}
      */
-    getSkills(): Promise<Skill[]> {
-        const skillsUrl = 'http://localhost:3000/data/skills.json';
+    find(): Observable<Developer> {
+        const url = 'http://localhost:3000/data/skills.json';
 
-        return this.http.get(skillsUrl)
-            .toPromise()
-            .then(response => response.json().data.skills as Skill[])
-            .catch(this.handleError);
-    }
-
-    /**
-     * Handle Http Get Error
-     * @param error
-     * @returns {Promise<void>|Promise<T>}
-     */
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
+        return this.http.get(url)
+          .map((res: Response) => <Developer>res.json().data);
     }
 }
