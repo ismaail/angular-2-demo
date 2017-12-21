f42636f2ed589caf280d03465ea99d0398d3889d
ismaail <contact@ismaail.com>
2017-12-21 22:32:45 +0000

    Move app/ to src/ folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: '#my-app',
+    selector: 'my-app',
     template: `
         <h1>Angular 2 - Demo App</h1>
         <router-outlet></router-outlet>
